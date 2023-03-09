b954fd7f37312366e7ead3ff2da0ea96ae10c07a
Anastasia Danilova <anastasia.proekt@gmail.com>
2023-03-09 17:07:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Chart from "react-google-charts";
+
+export const data = [
+  ["Вид поведениея", "Частота возникновения в процентах"],
+  ["Сотрудничество", 19],
+  ["Копромисс", 39],
+  ["Избегание", 16],
+  ["Приспособление", 23],
+  ["Соперничество", 3],
+];
+
+export const options = {
+  title: "Способы поведения в конфликтной ситуации (опросник Томаса)",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Chart
+  
+  chartType="PieChart"
+  data= { data }
+  options = { options }
+  width="100%"
+  height="400px"
+  legendToggle>
+
+  </Chart>
     </div>
   );
 }
